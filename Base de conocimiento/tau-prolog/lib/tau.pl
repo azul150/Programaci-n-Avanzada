4f45fc011413f62ce9e788494f2f07f5b130d1ce
Ivan Enrique Cruz Acosta <133553913+azul150@users.noreply.github.com>
2025-09-30 21:03:14 -0600

    Add files via upload

---
+++ Base de conocimiento/tau-prolog/lib/tau.pl
@@ -0,0 +1,73 @@
+:- module(tau, ['$member'/2, '$findall'/4, '$bagof'/3, '$setof'/3, '$if'/3]).
+
+:- meta_predicate
+    '$findall'(?, 0, -, ?),
+    '$bagof'(?, ^, -),
+    '$setof'(?, ^, -),
+    '$if'(0, 0, 0).
+
+%!  '$member'(?Element, ?List)
+%
+%   True if Element is a member of List.
+
+'$member'(X, [X|_]).
+'$member'(X, [_|Xs]) :- '$member'(X, Xs).
+
+%!  '$bind_bagof_keys'(+VarsTemplPairs, -Variables)
+%
+%   Establish a canonical binding of the variables.
+
+'$bind_bagof_keys'([], _).
+'$bind_bagof_keys'([Key-_|Bag], Vars) :-
+    term_variables(Key, Vars, _),
+    '$bind_bagof_keys'(Bag, Vars).
+
+%!  '$findall'(+Template, :Goal, -Bag, +Tail)
+%
+%   True if Bag is a list of values in the form Template that would make the
+%   goal Goal succeed. Tail is the tail of the difference list Bag-Tail.
+
+'$findall'(Template0, Goal0, Instances, Tail) :-
+    copy_term(Template0-Goal0, Template1-Goal1),
+    call(Goal1),
+    copy_term(Template1, Template2),
+    '$push_global_stack'(Var, Template2),
+    false ; '$flush_global_stack'(Var, Instances, Tail).
+
+%!  '$bagof'(+Template, :Goal, -Bag)
+%
+%   Unify Bag with the alternatives of Template for the goal Goal.
+
+'$bagof'(Template, Goal0, Answer) :-
+    '$free_variable_set'(Template^Goal0, Goal1, FV),
+    findall(FV-Template, Goal1, Answers, []),
+    '$bind_bagof_keys'(Answers, _),
+    keygroup(Answers, KeyGroups),
+    keysort(KeyGroups, KeySorted),
+    '$member'(FV-Answer, KeySorted).
+
+%!  '$setof'(+Template, :Goal, -Bag)
+%
+%   Equivalent to '$bagof'/3, but sorts the result using sort/2 to get a sorted
+%   list of alternatives without duplicates.
+
+'$setof'(Template, Goal0, Answer) :-
+    '$free_variable_set'(Template^Goal0, Goal1, FV),
+    findall(FV-Template, Goal1, Answers, []),
+    '$bind_bagof_keys'(Answers, _),
+    keygroup(Answers, KeyGroups),
+    keysort(KeyGroups, KeySorted),
+    '$member'(FV-Unsorted, KeySorted),
+    sort(Unsorted, Answer).
+
+%!  '$if'(:Condition, :Action, :Else)
+%
+%   This construct implements the so-called soft-cut.
+
+'$if'(If, Then, Else) :-
+    ( call(If),
+      '$push_global_stack'(Stack, _),
+      call(Then)
+    ; '$flush_global_stack'(Stack, [], []),
+      call(Else)
+    ).
